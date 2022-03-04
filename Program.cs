7427ca5ad42249662e170be52732ff3fac677358
mabbuprem <mabbupremkumar@gmail.com>
2022-03-04 12:32:13 +0530

    creating binary tree

--- Program.cs
+++ Program.cs
@@ -14,20 +14,6 @@ namespace Binary_Search_Trees
             bst.Insert(56);
             bst.Insert(30);
             bst.Insert(70);
-            bst.Insert(22);
-            bst.Insert(40);
-            bst.Insert(60);
-            bst.Insert(95);
-            bst.Insert(11);
-            bst.Insert(65);
-            bst.Insert(3);
-            bst.Insert(16);
-            bst.Insert(63);
-            bst.Insert(67);
-            bst.size();
-            //Searching the element in the Binary Tree.
-            bst.Search(63);
-            bst.Search(69);
         }
     }
 }
